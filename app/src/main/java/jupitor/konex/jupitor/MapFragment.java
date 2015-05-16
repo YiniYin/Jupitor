c6fa61d1465794bb9d5ec62bd0a0b33132663425
YiniYin <yiniski@gmail.com>
2015-05-16 23:14:08 +1200

    add google play services location

--- app/src/main/java/jupitor/konex/jupitor/MapFragment.java
+++ app/src/main/java/jupitor/konex/jupitor/MapFragment.java
@@ -1,22 +1,63 @@
 package jupitor.konex.jupitor;
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.SphericalUtil;
 
-public class MapFragment extends Fragment {
-    private GoogleMap mMap;
+public class MapFragment extends Fragment implements SensorEventListener, GoogleApiClient.ConnectionCallbacks
+        ,GoogleApiClient.OnConnectionFailedListener,LocationListener,OnMapReadyCallback {
+    private final String TAG = "JupiterMap";
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private static boolean magicCameraOn = false;
+    private static boolean searchRadiusOn = false;
+    private static LatLng mLatLng;
+    private static final float SEARCH_RADIUS = 700;
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private SensorManager mSensorManager;
+    private Sensor mVectorSensor;
+    private float[] mRotationMatrix = new float[16];
+    private float[] mValues = new float[3];
+    private float mAzimuth;
+    private float mTilt;
+    private double TARGET_OFFSET_METERS = 10;
+
+    private TextView mMessageView;
+
+    private ValueAnimator vAnimator;
+    private Circle mCircle;
+
     private OnFragmentInteractionListener mListener;
 
     public static MapFragment newInstance(int sectionNumber) {
@@ -42,17 +83,27 @@ public class MapFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient = new GoogleApiClient.Builder(this.getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
     }
 
     @Override
     public void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+    }
+
+    @Override
+    public void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
@@ -87,16 +138,105 @@ public class MapFragment extends Fragment {
         public void onFragmentInteraction(Uri uri);
     }
 
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
     private void setUpMapIfNeeded() {
         if (mMap == null) {
             mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
             if (mMap != null) {
                 setUpMap();
             }
+            SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
         }
     }
 
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection has been suspend");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "GoogleApiClient connection has failed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //mLocationView.setText("Location received: " + location.toString());
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (mLatLng != null && checkReady()) {
+            SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
+            SensorManager.getOrientation(mRotationMatrix, mValues);
+            mAzimuth = (float) Math.toDegrees(mValues[0]);
+            mTilt = (float) Math.toDegrees(mValues[1]);
+
+            CameraPosition cameraPosition = CameraPosition.builder().
+                    tilt(clamp(0,mTilt,(float)67.5)).
+                    bearing(mAzimuth).
+                    zoom(13+5*(mTilt/90)).
+                    target(SphericalUtil.computeOffset(mLatLng, TARGET_OFFSET_METERS, mAzimuth)).
+                    build();
+            mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+//    @Override
+//    public void onDisconnected() {
+//        // Do nothing
+//    }
+
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    private boolean checkReady() {
+        if (mMap == null) {
+            Toast.makeText(this.getActivity(), R.string.map_not_ready, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    public static float clamp(float x, float min, float max) {
+        if (x > max) return max;
+        if (x < min) return min;
+        return x;
     }
 }
