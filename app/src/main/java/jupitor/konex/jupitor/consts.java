148de42a458b127f477cf458a8ed5dfc9a3cdf56
YiniYin <yiniski@gmail.com>
2015-05-23 23:47:54 +1200

    move geolocation camera to release region

--- app/src/main/java/jupitor/konex/jupitor/consts.java
+++ app/src/main/java/jupitor/konex/jupitor/consts.java
@@ -1,7 +1,12 @@
 package jupitor.konex.jupitor;
 
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class consts {
     public static String dbPath = "/data/data/jupitor.konex.jupitor/databases/jupitor.db";
     public static String dbName = "jupitor.db";
+    public static double auckland_latitude = -36.848189;
+    public static double auckland_longitude = 174.762816;
+    public static LatLng auckland_geolocation = new LatLng(auckland_latitude, auckland_longitude);
 }
